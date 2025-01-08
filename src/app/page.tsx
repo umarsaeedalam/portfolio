b968705b75eb8d9fd81108819ed1b1f5f3685b42
“Umar <umarsaeedalam@gmail.com>
2025-01-08 21:33:19 +0500

    Major changes Part 9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ function HomePage() {
         <>
             <section className="flex justify-center items-center h-[calc(100svh-64px)] max-[1000px]:h-[calc(100svh-60px)]">
                 <div className="flex flex-col gap-5">
-                    <div className="flex items-center gap-3 max-[1000px]:gap-2">
-                        <p className="text-secondary-700 tracking-wide text-4xl max-[1000px]:text-3xl max-[800px]:text-2xl max-[625px]:text-22">Hi I&apos;m</p>
+                    <div className="flex items-center gap-3 max-[1100px]:gap-2">
+                        <p className="text-secondary-700 font-bold tracking-wide text-4xl max-[1100px]:text-3xl max-[850px]:text-2xl max-[650px]:text-22 max-[500px]:text-xl max-[400px]:text-lg">Hi I&apos;m</p>
 
-                        <div className="grow h-1 max-[800px]:h-[0.2rem] max-[625px]:h-[0.125rem] bg-accent-500"></div>
+                        <div className="grow h-1 max-[850px]:h-[0.2rem] max-[650px]:h-[0.125rem] bg-accent-500"></div>
                     </div>
 
-                    <h1 className={`${accentFont.className} text-12xl max-[1000px]:text-12xl max-[800px]:text-10xl max-[625px]:text-9xl max-[550px]:text-6xl tracking-normal leading-[115px] max-[1000px]:leading-[95px] max-[800px]:leading-[75px] max-[625px]:leading-[75px] max-[550px]:leading-[50px] text-secondary-500`}>Umar Saeed</h1>
+                    <h1 className={`${accentFont.className} text-11xl max-[1100px]:text-9xl max-[850px]:text-8xl max-[650px]:text-7xl max-[500px]:text-6xl max-[400px]:text-[54px] tracking-normal max-[1100px]:leading-normal max-[850px]:leading-[135px] max-[650px]:leading-[95px] max-[500px]:leading-[70px] max-[400px]:leading-[58px] text-secondary-500`}>Umar Saeed</h1>
 
-                    <h2 className="text-[54px] max-[1000px]:text-[46px] max-[800px]:text-4xl max-[625px]:text-3xl max-[550px]:text-2xl tracking-normal mt-5 max-[1000px]:mt-4 max-[800px]:mt-6 max-[550px]:-mt-2 text-accent-500 self-end">Next.Js Developer</h2>
+                    <h2 className="font-bold text-5xl max-[1100px]:text-4xl max-[850px]:text-3xl max-[650px]:text-2xl max-[500px]:text-xl max-[400px]:text-lg tracking-wide -mt-[75px] max-[1100px]:-mt-[60px] max-[850px]:-mt-[45px] max-[650px]:-mt-[35px] max-[500px]:-mt-[25px] max-[400px]:-mt-[26px] text-accent-500 self-end">Next.Js Developer</h2>
                 </div>
             </section>
 
